607ec1a87542ff01b7cc0d66127ea5e5aba609a4
ivlup <ivlupsecond@gmail.com>
2021-06-09 16:17:42 +0200

    SPO-5857 Firebase integration - Changed dependencies - Added lint - Added firebase connection

---
+++ lib/ui/app/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class App extends StatefulWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'BoardManager',
+      home: Scaffold(),
+    );
+  }
+}
